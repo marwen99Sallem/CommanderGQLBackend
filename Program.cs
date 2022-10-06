fd9fd12aca0c2dba37d6bd5513a9bcdce99bfeb6
Marwen Sallem <marwensallem99@gmail.com>
2022-10-07 00:43:16 +0100

    sql setup

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// this is where the dependency injection registration is accomplished (instead of startup.cs file in previous versions)
+
+
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
---
+++ Startup.cs
@@ -0,0 +1,44 @@
+﻿using CommanderGQL.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CommanderGQL;
+
+    public class Startup
+    {
+    public IConfiguration Configuration { get; }
+
+    public Startup(IConfiguration configuration)
+        {
+           Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+         {
+
+        services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer
+        (Configuration.GetConnectionString("CommandConStr")));
+
+         }
+
+
+
+        public void Configure(IApplicationBuilder app,IWebHostEnvironment env)
+        {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseWebSockets();
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGraphQL();
+        });
+
+       
+    }
+
+    }
